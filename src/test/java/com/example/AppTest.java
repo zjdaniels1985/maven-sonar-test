5c9ea03cad14881edbf7dac907c68f5596cce800
Zachary Daniels <zjdaniels1985@live.com>
2025-02-20 21:35:53 -0600

    Initial Commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
